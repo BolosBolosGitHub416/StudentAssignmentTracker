22743af4fc4f2efad10f7dbe2bb75c1a23f9f764
brandonpagani1 <brandon_pagani@hotmail.com>
2026-04-21 21:48:39 -0400

    Update announcements.js
    - Added reference to User model for announcement creator
    - Introduced indexing on createdBy for faster queries
    - Ensured required fields for title and message
    - Structured schema for scalability and future filtering

--- server/models/announcements.js
+++ server/models/announcements.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 
-const AnnouncementSchema = new mongoose.Schema({
+const announcementSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
   course: String,
-  message: String,
-  createdAt: { type: Date, default: Date.now }
-});
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model("Announcement", AnnouncementSchema);
+module.exports = mongoose.model("Announcement", announcementSchema);
